a60207a04dc682cccc1ce1435dbb399305cde35a
unknown <Niromedium@gmail.com>
2022-08-29 10:22:35 +0500

    add Задача21

---
+++ Case21/Program.cs
@@ -0,0 +1,15 @@
+﻿// Принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве
+Console.WriteLine("Введите число х1: ");
+double x1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите число y1: ");
+double y1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите число z1: ");
+double z1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите число x2: ");
+double x2 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите число y2: ");
+double y2 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите число z2: ");
+double z2 = double.Parse(Console.ReadLine());
+double dist = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
+Console.WriteLine(dist);
