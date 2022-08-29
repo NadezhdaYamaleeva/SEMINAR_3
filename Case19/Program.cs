e33e46fd04aed2b668499d238d05d4ad058f13f1
unknown <Niromedium@gmail.com>
2022-08-29 10:06:04 +0500

    add Задача19

---
+++ Case19/Program.cs
@@ -0,0 +1,20 @@
+﻿// Принимает на вход пятизначное число и проверяет, является ли оно палиндромом
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+while(number < 10000 || number >= 100000)
+{
+    Console.WriteLine("Введите пятизначное число: ");
+    number = int.Parse(Console.ReadLine());
+}
+int a = number/10000;
+int b = number%10;
+int c = (number/1000)%10;
+int d = (number%100)/10;
+if (a == b && c == d)
+{
+    Console.WriteLine("Введенное число является палиндромом");
+}
+else
+{
+    Console.WriteLine("Введенное число не является палиндромом");
+}
